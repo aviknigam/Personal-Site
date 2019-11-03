739064570887e7282a4f6251d8e6896527500a7c
Avik Nigam <business.aviknigam@gmail.com>
2019-11-04 09:26:30 +1000

    test

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require __DIR__ . '/core/init.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Avik Nigam</h1>
+    <h2>Medical Student and Software Developer based in Gold Coast, Australia</h2>
+    
+</body>
+</html>
