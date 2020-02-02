0b2c988ba3c31d7b67fbc4513e52723f0fac42a5
Avik Nigam <business.aviknigam@gmail.com>
2020-02-03 01:58:09 +1000

    Added navbar

---
+++ includes/navbar.php
@@ -0,0 +1,14 @@
+<nav>
+	<div class="container">
+		<div class="navbar">
+			<a class="navbar-brand" href="/">
+				<img src="/dist/img/logo.png" class="navbar-brand-logo" alt="">
+				<span class="navbar-brand-text"><?= $configTitle; ?></span>
+			</a>
+			<div class="navbar-links">
+				<a href="/about">About</a>
+				<a href="/blog">Blog</a>
+			</div>
+		</div>
+	</div>
+</nav>
