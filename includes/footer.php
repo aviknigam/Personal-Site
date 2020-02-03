df991eaf4337d974e414dd8318c70a9623daa326
Avik Nigam <business.aviknigam@gmail.com>
2020-02-03 19:27:10 +1000

    unfinished footer

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<div class="footer">
+	<span>&copy; <?= date('Y'); ?> Avik Nigam</span>
+	<span>View <a href="https://github.com/aviknigam/Personal-Site/blob/master/LICENSE" target="_blank" class="text-light-grey"><u>MIT Licence</u></a> on GitHub <i class="fa fa-github fa-fw fa-lg" aria-hidden="true"></i></span>
+</div>
